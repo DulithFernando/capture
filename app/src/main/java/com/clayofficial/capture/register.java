1ae46d8bc795771511bf0a826a68a3657a73642f
Manuka Prabath <manukaprabath.bk@gmail.com>
2017-06-02 03:05:59 +0530

    registration

--- app/src/main/java/com/clayofficial/capture/register.java
+++ app/src/main/java/com/clayofficial/capture/register.java
@@ -9,6 +9,19 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity implements View.OnClickListener {
     private TextView haveacc;
@@ -36,6 +49,42 @@ public class register extends AppCompatActivity implements View.OnClickListener 
         //Listener
         haveacc.setOnClickListener(this);
         reg_btn.setOnClickListener(this);
+    }
+
+    public void register()
+    {
+        final String email= mail.getText().toString().trim();
+        final String fullname=name.getText().toString().trim();
+        final String pass=password.getText().toString().trim();
+        loader.show();
+        StringRequest webrequest=new StringRequest(Request.Method.POST, "http://clayofficial.com/mad/v1/registerUser.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                loader.dismiss();
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loader.hide();
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params=new HashMap<>();
+                params.put("email",email);
+                params.put("password",pass);
+                params.put("name",fullname);
+                return params;
+            }
+        };
+        RequestHandler.getInstance(this).addToRequestQueue(webrequest);
     }
 
     @Override
